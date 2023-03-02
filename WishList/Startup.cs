2cb4165f76c0cca8ae0c415ac0c4a3de4f32276a
JonerVincentDoroy <jonervincent@yahoo.com>
2023-03-02 12:18:11 -0700

    Modified spacing
    Modified spacing for the test to marked complete properly

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -22,8 +22,7 @@ namespace WishList
 
             //configure database to point Entity Framework to the application's 
             //DbContext
-            services.AddDbContext<ApplicationDbContext>(options
-                => options.UseInMemoryDatabase("WishList"));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("WishList"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
