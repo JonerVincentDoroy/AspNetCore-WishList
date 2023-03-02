9e81eadb586d00e0d69c71121957e78b8c7a5ed8
JonerVincentDoroy <jonervincent@yahoo.com>
2023-03-02 11:39:46 -0700

    Added comments

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -4,11 +4,13 @@ namespace WishList.Controllers
 {
     public class HomeController : Controller
     {
+        //call Index page if requested
         public IActionResult Index()
         {
             return View("Index");
         }
 
+        //call Error page if requested
         public IActionResult Error() 
         {
             return View("Error");
