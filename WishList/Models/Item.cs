9077fa04c91fcdfbfec6c889c9764d412a160f1a
JonerVincentDoroy <jonervincent@yahoo.com>
2023-03-02 12:14:05 -0700

    Comments, Models, DBContext
    Added some few comments, created and set up entity framework DB, added an Item class in Model folder

---
+++ WishList/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations; //to add attributes to a property
+
+namespace WishList.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        //added attributes to Description property
+        [Required]
+        [MaxLength(50)]
+        public string Description { get; set; }
+
+    }
+}
